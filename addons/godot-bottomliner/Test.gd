ad199449869566a8877db72f3ffff5c922187c9f
Firepal <badbrad@outlook.fr>
2022-10-01 22:02:49 +0200

    progress, mein freind

--- addons/godot-bottomliner/Test.gd
+++ addons/godot-bottomliner/Test.gd
@@ -12,6 +12,7 @@ func set_ex_points(val):
 
 func _ready():
 	p.connect("curve_changed",self,"_update")
+	m = $MeshInstance
 	_update()
 
 func _gen_mesh(points : PoolVector3Array):
@@ -19,7 +20,6 @@ func _gen_mesh(points : PoolVector3Array):
 		return ArrayMesh.new()
 	
 	var dir = Vector3.UP
-	var last_p = null
 	
 	var _v = []
 	var _n = []
@@ -34,9 +34,17 @@ func _gen_mesh(points : PoolVector3Array):
 	_c.resize(_v.size())
 	_uv.resize(_v.size())
 	_uv2.resize(_v.size())
-	_i.resize( (points.size() * (3*3)) + 6 )
+	_i.resize( (points.size() * (8)) + 6 )
 	
 	var inv_ps = 1.0/(points.size()+1)
+	
+	var line_length_sq = 0.0
+	for j in range(points.size()-1):
+		line_length_sq += points[j].distance_squared_to(points[j+1])
+	var line_length = sqrt(line_length_sq)
+	var inv_line_length = 1.0 / line_length
+	print("length: ", line_length)
+	print("length invsqrt: ", inv_line_length)
 	
 	for i in range(points.size()):
 		var p1 : Vector3 = points[i]
@@ -58,9 +66,7 @@ func _gen_mesh(points : PoolVector3Array):
 		_n[i+2] = dir
 		
 		# last point
-		var lp = p1
-		if last_p != null:
-			lp = last_p
+		var lp = points[max((i-1)-points.size(),0)]
 		
 		# can't store negative values in color
 		# so we use plane method
@@ -70,20 +76,23 @@ func _gen_mesh(points : PoolVector3Array):
 		var ip = i
 		if ip != 0: ip += 1
 		for _u in range(5):
-			print("tangent ", ip)
 			_t[ip] = lpp.normal.x
 			_t[ip+1] = lpp.normal.y
 			_t[ip+2] = lpp.normal.z
 			_t[ip+3] = 1.0
 			ip += 4
 		
-		var lpl = Color(lpp.d * 0.1,lpp.d * 0.4,lpp.d, (lpp.d - 3.0) * 0.1)
+		var lpl = Color(
+			lpp.d * 0.1,
+			lpp.d * 0.4,
+			lpp.d,
+			0.0
+		)
 		
 		_c[i] = lpl
 		_c[i+1] = lpl
 		_c[i+2] = lpl
 		
-		last_p = p1
 		
 		print( "yes ", inv_ps*i )
 		print( _t)
@@ -94,70 +103,52 @@ func _gen_mesh(points : PoolVector3Array):
 		_uv[i+1] = Vector2(0.5,-0.288671)
 		_uv[i+2] = Vector2(0,0.577342)
 		
-		var uv_x = inv_ps*i
+		var uv2_v = Vector2(inv_ps*i, inv_line_length)
 		
-		_uv2[i  ] = Vector2(uv_x,0)
-		_uv2[i+1] = Vector2(uv_x,0)
-		_uv2[i+2] = Vector2(uv_x,0)
+		_uv2[i  ] = uv2_v
+		_uv2[i+1] = uv2_v
+		_uv2[i+2] = uv2_v
 		
 		if i_ < points.size()-1:
-			print( i )
 			var e = i
 			
 			# Start cap
-			if i == 0:
+			if i_ == 0:
 				_i[i] = e+2
 				_i[i+1] = e+1
 				_i[i+2] = e
-			else:
-				i += i_ * 3
-			
-			# Offset for start cap
-			i += 3
-			
-			# TODO: Second and third iterations create bad indices
-			# Only bottom side correctly renders with more than two points
-			# Help ¨n
-			
-			for u in range(3):
-				var c = e+u
-				var cn = c+3
-				
-				var n = c+1
-				var nn = n+3
-				
-				print("SIDE: ", u)
-				printt(c, n, points.size() * 3)
-				
-				_i[i] = n
-				_i[i+1] = cn
-				_i[i+2] = c
-				
-				_i[i+3] = n
-				_i[i+4] = nn
-				_i[i+5] = cn
-				i += 6
-			print( _i )
-			
-			
-#			var c = e+1
-#			var n = c+3
-#			printt(c, n, points.size() * (3))
-#			_i[i] = c
-#			_i[i+1] = c + 1
-#			_i[i+2] = n
-#
-#			_i[i+3] = c + 1
-#			_i[i+4] = n + 1
-#			_i[i+5] = n
-#			i += 6
-#
-			# End cap
-			if i_ == points.size()-2:
+			elif i_ == points.size()-2:
 				_i[i] = e+3
 				_i[i+1] = e+4
 				_i[i+2] = e+5
-		
+
+			# Offset for start cap
+			i += 3
+
+			# TODO: Second and third iterations create bad indices
+			# Only bottom side correctly renders with more than two points
+			# Help ¨n
+
+			var it = i
+			var ey = e
+			for u in range(3):
+				var t1 = ey+(u%3)
+				var t2 = ey+((u + 1) % 3)
+				
+				var t3 = t1 + 3
+				var t4 = t2 + 3
+				
+				_i[it] = t1
+				_i[it+1] = t2
+				_i[it+2] = t4
+				
+				_i[it+3] = t1
+				_i[it+4] = t4
+				_i[it+5] = t3
+				it += 6
+	
+	print( _i )
+	
 	var mesh = ArrayMesh.new()
 	
 	var arrs = []
@@ -170,9 +161,10 @@ func _gen_mesh(points : PoolVector3Array):
 	arrs[ArrayMesh.ARRAY_TEX_UV2] = PoolVector2Array(_uv2)
 	arrs[ArrayMesh.ARRAY_INDEX] = PoolIntArray(_i)
 	
-	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES,arrs,[],ArrayMesh.ARRAY_COMPRESS_DEFAULT - ArrayMesh.ARRAY_COMPRESS_TANGENT)
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES,arrs,[],ArrayMesh.ARRAY_COMPRESS_DEFAULT - ArrayMesh.ARRAY_COMPRESS_TANGENT - ArrayMesh.ARRAY_COMPRESS_COLOR)
 	
 	return mesh
 
 func _update():
-	m.mesh = _gen_mesh(ex_points)
+	if is_inside_tree():
+		m.mesh = _gen_mesh(ex_points)
